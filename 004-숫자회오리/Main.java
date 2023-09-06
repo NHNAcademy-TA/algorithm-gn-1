76f6c3393b26cd3aed6a410c6dfa9846713682ba
sudang <tidh1640@naver.com>
2023-09-06 22:33:21 +0900

    [004] 숫자회오리 Test 코드 수정

--- 004-숫자회오리/Main.java
+++ 004-숫자회오리/Main.java
@@ -2,9 +2,9 @@ import java.util.Arrays;
 
 public class Main {
 
-    private static final int[] TEST_CASES = { 4, 5, 10 };
+    private static final int[] TESTCASES = { 4, 5, 10 };
 
-    private static final int[][][] TEST_CASES_RESULT = {
+    private static final int[][][] TESTCASES_RESULT = {
         {
             {1, 2, 3, 4},
             {12, 13, 14, 5},
@@ -35,10 +35,10 @@ public class Main {
     private static double correct = 0;
     
     public static void main(String[] args) {
-        for (int i = 0; i < TEST_CASES.length; i++) 
-            System.out.println("Test Case " + (i + 1) + " = " + test(TEST_CASES[i], TEST_CASES_RESULT[i]));
+        for (int i = 0; i < TESTCASES.length; i++) 
+            System.out.println("Test Case " + (i + 1) + " = " + test(TESTCASES[i], TESTCASES_RESULT[i]));
 
-        System.out.printf("정답률 = %.3f%%", (correct / TEST_CASES.length * 100));
+        System.out.println("정답률 = " + (int) (correct / TESTCASES.length * 100) + "%");
     }
 
     private static boolean test(int input, int[][] result) {
